b965429dee31bcaf1ecbcdb826dd66a1fc149623
ipiyou <tao710803@gmail.com>
2022-02-14 03:13:41 +0900

    React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,11 @@
-import React, { useRef,useState } from 'react';
+import React, { useMemo, useRef,useState } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
+
+function countActiveUsers(users){
+  console.log('활성 사용자 수를 세는중...');
+  return users.filter(user => user.active).length;
+} 
 
 function App() {
   const [inputs, setInputs] = useState({
@@ -21,17 +26,20 @@ function App() {
     {
       id: 1,
       username: 'velopert',
-      email: 'public.velopert@gmail.com'
+      email: 'public.velopert@gmail.com',
+      active: true
     },
     {
       id: 2,
       username: 'tester',
-      email: 'tester@example.com'
+      email: 'tester@example.com',
+      active: false
     },
     {
       id: 3,
       username: 'liz',
-      email: 'liz@example.com'
+      email: 'liz@example.com',
+      active: false
     }
   ]);
 
@@ -56,7 +64,15 @@ function App() {
   const onRemove = id => {
     setUsers(users.filter(user => user.id !== id));
   }
+  const onToggle = id => {
+    setUsers(
+      users.map(user =>
+        user.id === id ? {...user, active: !user.active} : user
+      )
+    );
+  };
 
+  const count = useMemo(() => countActiveUsers(users),[users]);
   return (
     <>
       <CreateUser username = {username}
@@ -64,7 +80,8 @@ function App() {
       onChange = {onChange}
       onCreate = {onCreate}
       />
-      <UserList users={users} onRemove={onRemove} />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+      <div>활성 사용자수 : {count}</div>
     </>
   );
 }
