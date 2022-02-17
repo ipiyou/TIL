cf0fc042a3a5c9f52cd782041585fa5480d6e4ef
ipiyou <tao710803@gmail.com>
2022-02-18 01:01:16 +0900

    style-components

--- my-app/styled-c/App.js
+++ my-app/styled-c/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
+import Button from './Button';
 
-const Circle = styled.div`
-width: 5rem;
-height: 5rem;
-background: ${props => props.color || 'black'};
-border-radius: 50%;
+const AppBlock = styled.div`
+width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
 `;
 
 function App() {
-  return <Circle color="blue" />;
+  return (
+  <AppBlock>
+    <Button>BUTTON</Button>
+  </AppBlock>
+  );
 }
 
 export default App;
