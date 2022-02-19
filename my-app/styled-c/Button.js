4d53195fca84ea69c75edfc525f619422f0ac664
ipiyou <tao710803@gmail.com>
2022-02-19 23:33:50 +0900

    styled-components

--- my-app/styled-c/Button.js
+++ my-app/styled-c/Button.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { darken, lighten } from 'polished';
 
 const StyledButton = styled.button`
   /* 공통 스타일 */
@@ -13,17 +14,25 @@ const StyledButton = styled.button`
   padding-left: 1rem;
   padding-right: 1rem;
 
+  /* 크기 */
   height: 2.25rem;
   font-size: 1rem;
 
-  background: #228be6;
-  &:hover {
-    background: #339af0;
-  }
-  &:active {
-    background: #1c7ed6;
-  }
+  /* 색상 */
+  ${props => {
+    const selected = props.theme.palette[props.color];
+    return css`
+      background: ${selected};
+      &:hover {
+        background: ${lighten(0.1, selected)};
+      }
+      &:active {
+        background: ${darken(0.1, selected)};
+      }
+    `;
+  }}
 
+  /* 기타 */
   & + & {
     margin-left: 1rem;
   }
@@ -32,5 +41,9 @@ const StyledButton = styled.button`
 function Button({ children, ...rest }) {
   return <StyledButton {...rest}>{children}</StyledButton>;
 }
+
+Button.defaultProps = {
+  color: 'blue'
+};
 
 export default Button;
